023f6825bf10744632a912f7638678102d573096
yuange <kaikai666>
2020-10-19 13:21:04 +0800

    第一次提交

---
+++ src/main/java/com/company/employlocation/EmployLocationApplication.java
@@ -0,0 +1,13 @@
+package com.company.employlocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployLocationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployLocationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/employlocation/EmployLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.employlocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployLocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
